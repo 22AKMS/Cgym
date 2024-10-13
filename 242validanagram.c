d4818961648b1c100f67ad48ff5b4f70b667f491
Abdulla Alsaadi <48903419+22AKMS@users.noreply.github.com>
2024-10-13 09:53:47 -0400

    13th oct

---
+++ 242validanagram.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool isAnagram(char* s, char* t) {
+    int count[26] = {0};
+
+    if (strlen(s) != strlen(t)) {
+        return false;
+    }
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        count[s[i] - 'a']++;
+        count[t[i] - 'a']--;
+    }
+
+    for (int i = 0; i < 26; i++) {
+        if (count[i] != 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void main (){
+
+    char s[]="rac", t[]="car";
+    isAnagram(s,t);
+}
